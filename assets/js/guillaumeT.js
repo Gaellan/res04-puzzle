300c1a95eddd65d8976d71c18783e08a3b595914
TessierGuillaume <130038087+TessierGuillaume@users.noreply.github.com>
2023-06-21 15:37:09 +0200

    mise a jour puzzle9

--- assets/js/guillaumeT.js
+++ assets/js/guillaumeT.js
@@ -1,12 +1,13 @@
 
 
 
-window.addEventListener("DOMContentLoaded", function(){
-    let images = [209, 427, 311, 439, 115, 456, 153, 6, 97, 152, 332, 3, 281, 225, 327, 366, 399, 375, 
-        444, 42, 255, 238, 348, 39, 434, 286, 79, 254, 87, 411, 54, 139, 67, 287, 103, 45, 261, 149, 171];
+
+    
 
     function Puzzler9()
     {
+        let images = [209, 427, 311, 439, 115, 456, 153, 6, 97, 152, 332, 3, 281, 225, 327, 366, 399, 375, 
+            444, 42, 255, 238, 348, 39, 434, 286, 79, 254, 87, 411, 54, 139, 67, 287, 103, 45, 261, 149, 171];
         let positionColonne;
         let positionLigne;
         const grid = document.querySelector('main');
@@ -27,7 +28,7 @@ window.addEventListener("DOMContentLoaded", function(){
         }
     }
 
-    Puzzler9();
-});
+    
+  export {Puzzler9};
 
 
