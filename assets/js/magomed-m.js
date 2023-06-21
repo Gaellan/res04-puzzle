0cfcbd3bdc0ea0374d4e7b1a6f2d25df03949d83
Magomed35 <magomed35@icloud.com>
2023-06-21 15:47:21 +0200

    puzzler6

--- assets/js/magomed-m.js
+++ assets/js/magomed-m.js
@@ -1,23 +1,32 @@
-// section background-image 
+window.addEventListener("DOMContentLoaded", function(){
+    
 
-function render6()
-{
-    let images = [442, 446, 258, 179, 197, 88, 425, 347, 340, 318, 37, 119, 120, 185, 200, 376, 216, 84, 246, 226, 426, 26, 408, 211, 128, 213, 453, 420, 242, 259, 180, 35, 32, 379, 154, 190, 441, 100, 78];
-    const cols = 24;
-    // const rows = 19;
-
-    images.forEach(position =>
+    function Puzzler6()
     {
-        const colImage = Math.floor(position / cols) + 1;
-        const rowImage = position % cols;
+        let images = [442, 446, 258, 179, 197, 88, 425, 347, 340, 318, 37, 119, 120, 185, 200, 376, 216, 84, 246, 226, 426, 26, 408, 211, 128, 213, 453, 420, 242, 259, 180, 35, 32, 379, 154, 190, 441, 100, 78];
 
+
+        let positionColonne;
+        let positionLigne;
         const grid = document.querySelector('main');
-        const urlImg = `assets/images/spicegirls_row-${rowImage}-column-${colImage}.jpg`;
 
-        const region = document.createElement('section');
-        region.style.backgroundImage = `url(\"${urlImg}\")`;
-        region.style.gridColumn = `${colImage}`;
-        region.style.gridRow = `${rowImage}`;
-        grid.appendChild(region);
-    })
-}
+        for(let nbr of images){
+            positionColonne = nbr % 19;
+            if(positionColonne === 0){
+                positionColonne = 19;
+            }
+            positionLigne = Math.ceil(nbr / 19);
+            
+            const urlImg = `assets/images/spicegirls_row-${positionLigne}-column-${positionColonne}.jpg`;
+            const region = document.createElement('section');
+            region.style.backgroundImage = `url(\"${urlImg}\")`;
+            region.style.gridColumn = `${positionColonne}`;
+            region.style.gridRow = `${positionLigne}`;
+            grid.appendChild(region);
+        }
+    }
+
+    Puzzler6();
+});
+
+export { Puzzler6 };
